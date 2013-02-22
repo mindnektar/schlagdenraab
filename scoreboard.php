3fbb9b56481c44bcebb40045696f585a2ae108c2
Martin Denk <wyverns.spirit@gmail.com>
2013-02-22 16:55:19 +0100

    display camera frame on scoreboard page

--- scoreboard.php
+++ scoreboard.php
@@ -1,5 +1,6 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
+$cameraUrl = isset($_GET['camera']) ? $_GET['camera'] : 'http://localhost:8080';
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,6 +8,7 @@ header('Content-Type: text/html; charset=utf-8');
     <title>Schlag den Raab</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="css/scoreboard.css" />
     <link rel="stylesheet" type="text/css" href="js/plugins/scoreboard/jquery.scoreboard.css" />
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/plugins/scoreboard/jquery.scoreboard.js"></script>
@@ -14,7 +16,11 @@ header('Content-Type: text/html; charset=utf-8');
 </head>
 <body>
 
-<div id="scoreboard"></div>
+<div class="wrapper">
+    <iframe id="camera" src="<?php echo $cameraUrl; ?>" width="1280" height="720"></iframe>
+
+    <div id="scoreboard"></div>
+</div>
 
 </body>
 </html>
